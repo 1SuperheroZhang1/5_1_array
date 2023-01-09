6e143305a298b93753f95e88e8d7c08c10899530
zhangcy <2304417358@qq.com>
2023-01-09 20:21:53 +0800

    C语言督学营 - 5.1 数组的定义和一维数组在内存中的存储

---
+++ cmake-build-debug/CMakeFiles/5_1_array.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_1_array.exe"
+  "5_1_array.exe.manifest"
+  "5_1_array.pdb"
+  "CMakeFiles/5_1_array.dir/main.c.obj"
+  "lib5_1_array.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/5_1_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
